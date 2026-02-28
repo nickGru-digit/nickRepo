1b37d90010f41d709f74a312ef93f97605fd80b2
nickGru-digit <nick.grundman27@gmail.com>
2026-02-27 23:42:38 -0500

    fixed sword trail again?

--- docs/script.js
+++ docs/script.js
@@ -1,29 +1,28 @@
 let swords = [];
 
-            const cursor = document.getElementById("main");
+const cursor = document.getElementById("main");
 
-            document.addEventListener("mousemove", (e) => {
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX - (cursor.offsetWidth - 30) / 2 + "px";
+    cursor.style.top = e.clientY - (cursor.offsetHeight - 30) / 2 + "px";
+    const sword = document.createElement("div");
+    sword.style.pointerEvents = "none";
+    sword.classList.add("sword");
+    document.body.appendChild(sword);
 
-                cursor.style.left = e.clientX - (cursor.offsetWidth - 30) / 2 + "px";
-                cursor.style.top = e.clientY - (cursor.offsetHeight - 30) / 2 + "px";
-                const sword = document.createElement("div");
-                sword.classList.add("sword");
-                sword.style.pointerEvents = "none";
-                document.body.appendChild(sword);
-            
-                sword.style.left = e.clientX - (sword.offsetWidth - 30) / 2 + "px";
-                sword.style.top = e.clientY - (sword.offsetHeight - 30) / 2 + "px";
-            
-                swords.push(sword);
-            });
-            
-        setInterval(() => {
-            if (swords.length > 0) {
-                let sword = swords.shift();
-                sword.classList.add("fade");
-        
-                setTimeout(() => {
-                    sword.remove();
-                }, 250);
-            }
-        }, 5);
+    sword.style.left = e.clientX - (sword.offsetWidth - 30) / 2 + "px";
+    sword.style.top = e.clientY - (sword.offsetHeight - 30) / 2 + "px";
+
+    swords.push(sword);
+});
+    
+setInterval(() => {
+    if (swords.length > 0) {
+        let sword = swords.shift();
+        sword.classList.add("fade");
+
+        setTimeout(() => {
+            sword.remove();
+        }, 250);
+    }
+}, 5);
