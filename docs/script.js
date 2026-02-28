1bd21609c38c63062de8010892e68197fe9cf954
nickGru-digit <nick.grundman27@gmail.com>
2026-02-27 23:33:55 -0500

    finalized cursor trail

---
+++ docs/script.js
@@ -0,0 +1,29 @@
+let swords = [];
+
+            const cursor = document.getElementById("main");
+
+            document.addEventListener("mousemove", (e) => {
+
+                cursor.style.left = e.clientX - (cursor.offsetWidth - 30) / 2 + "px";
+                cursor.style.top = e.clientY - (cursor.offsetHeight - 30) / 2 + "px";
+                const sword = document.createElement("div");
+                sword.classList.add("sword");
+                sword.style.pointerEvents = "none";
+                document.body.appendChild(sword);
+            
+                sword.style.left = e.clientX - (sword.offsetWidth - 30) / 2 + "px";
+                sword.style.top = e.clientY - (sword.offsetHeight - 30) / 2 + "px";
+            
+                swords.push(sword);
+            });
+            
+        setInterval(() => {
+            if (swords.length > 0) {
+                let sword = swords.shift();
+                sword.classList.add("fade");
+        
+                setTimeout(() => {
+                    sword.remove();
+                }, 250);
+            }
+        }, 5);
