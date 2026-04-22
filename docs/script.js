1bf9d9e4bdae35c256a55cb4ee2bbd13da1d4307
nickGru-digit <nick.grundman27@gmail.com>
2026-04-22 16:50:25 -0400

    added annoying ass popup you can kill

--- docs/script.js
+++ docs/script.js
@@ -33,11 +33,55 @@
 const fireSound = new Audio("images/fire.mp3");
 const bubbleSound = new Audio("images/bubbles.mp3");
 
-document.addEventListener('click', () => {
-    fireSound.currentTime = 0;
-    fireSound.play();
-});
-
 window.onload = function() {
     bubbleSound.play()
 }
+
+var jumping = false;
+const boingSound = new Audio("images/boing.mp3");
+function jump() {
+    trampoline = document.getElementById("jumping");
+    trampoline.style.transition = "transform 1.25s ease";
+    if (jumping == false) 
+    { 
+        jumping = true;
+        boingSound.play();
+        trampoline.style.transform = "scale(1.5)";
+        setTimeout(() => {trampoline.style.transform = "scale(0.25)";}, 400);
+        setTimeout(() => {trampoline.style.transform = "scale(1)";}, 1700);
+        setTimeout(() => {jumping = false;}, 2500);
+    }
+}
+
+const errorSound = new Audio("images/error.mp3");
+var delay = 30;
+function freakout() {
+    popup = document.getElementById("popup");
+    const randomDegree = Math.floor(Math.random() * 20);
+    const oppDegree = -randomDegree;
+    
+    errorSound.currentTime = 0;
+    errorSound.playbackRate = Math.random() * 0.75 + 1;
+    errorSound.preservesPitch = false;
+    errorSound.play();
+    
+    popup.style.animationPlayState = "paused";
+    delay -= 0.5;
+    popup.style.animationDelay = `${delay}s`;
+    popup.style.animationPlayState = "running";
+    
+    popup.style.transition = "transform 0.125s ease";
+    popup.style.filter = "hue-rotate(135deg) blur(2px)";
+    popup.style.transform = `rotate(${randomDegree}deg)`;
+    setTimeout(() => {popup.style.transform =`rotate(${oppDegree}deg)`}, 125);
+    setTimeout(() => {popup.style.transform ="rotate(0deg)"}, 250);
+    setTimeout(() => {popup.style.filter = "hue-rotate(0deg) blur(0px)"}, 250);
+}
+
+document.addEventListener('click', () => {
+    if (errorSound.paused && boingSound.paused)
+    {
+        fireSound.currentTime = 0;
+        fireSound.play();
+    }
+});
